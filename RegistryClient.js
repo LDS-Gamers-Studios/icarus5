3cd6f0fa893d17bb067161364e68f2fa0704450d
Gaiwecoor <gaiwecoor@gmail.com>
2021-08-18 11:26:04 -0500

    This is dumb.

--- RegistryClient.js
+++ RegistryClient.js
@@ -1,11 +1,16 @@
 const config = require("./config/config.json");
 const Discord = require("discord.js");
 
-const client = new Discord.Client();
+const client = new Discord.Client({
+  intents: 0
+});
+
+const package = {client};
 
 client.on("ready", () => {
-  console.log(`Application Command Manager for ${client.user.username} is ready!`);
+  console.log(`Client for ${client.user.username} is ready!`);
+  package.commandManager = client.application.commands;
 })
 .login(config.token);
 
-module.exports = client.application.commands;
+module.exports = package;
