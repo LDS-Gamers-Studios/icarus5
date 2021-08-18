9303df294acbe7d1e1c64ed44f468583050d213c
Gaiwecoor <gaiwecoor@gmail.com>
2021-08-18 10:24:21 -0500

    Rewrite to use D.js native methods

--- RegistryClient.js
+++ RegistryClient.js
@@ -1,58 +1,11 @@
-const axios = require("axios");
+const config = require("./config/config.json");
+const Client = require("discord.js").Client;
 
-class RegistryClient {
-  constructor(applicationId, token) {
-    this.applicationId = applicationId;
-    this.token = token;
-  }
+const client = new Discord.Client();
 
-  async _call(url, data, method = "get") {
-    let response = await axios({
-      method,
-      url,
-      data,
-      baseURL: `https://discord.com/api/v8/applications/${this.applicationId}`,
-      headers: { Authorization: `Bot ${this.token}` }
-    });
-    return response.data;
-  }
+client.on("ready", () => {
+  console.log(`Application Command Manager for ${client.user.username} is ready!`);
+})
+.login(config.token);
 
-  batchEditGuildCommandPermissions(guildId, permissions) {
-    return this.call(`/guilds/${guildId}/commands/permissions`, permissions, "put");
-  }
-
-  bulkOverwriteCommands(commands, guildId) {
-    return this.call(`${guildId ? `/guilds/${guildId}` : ""}/commands`, commands, "put");
-  }
-
-  createCommand(command, guildId) {
-    return this._call(`${guildId ? `/guilds/${guildId}` : ""}/commands`, command, "post");
-  }
-
-  deleteCommand(commandId, guildId) {
-    return this._call(`${guildId ? `/guilds/${guildId}` : ""}/commands/${commandId}`, null, "delete");
-  }
-
-  editCommand(commandId, command, guildId) {
-    return this._call(`${guildId ? `/guilds/${guildId}` : ""}/commands/${commandId}`, command, "patch");
-  }
-
-  editGuildCommandPermissions(guildId, commandId, permissions) {
-    return this._call(`/guilds/${guildId}/commands/${commandId}/permissions`, permissions, "put");
-  }
-
-  getCommand(commandId, guildId) {
-    return this._call(`${guildId ? `/guilds/${guildId}` : ""}/commands/${commandId}`);
-  }
-
-  getGuildCommandPermissions(guildId, commandId) {
-    return this._call(`${guildId ? `/guilds/${guildId}` : ""}/commands${commandId ? `/${commandId}` : ""}/permissions`);
-  }
-
-  getCommands(guildId) {
-    return this._call(`${guildId ? `/guilds/${guildId}` : ""}/commands`);
-  }
-
-}
-
-module.exports = RegistryClient;
+module.exports = client.application.commands;
