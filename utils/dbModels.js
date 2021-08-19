8aa8bccc4445f28d1d50f42c8a2ec8664fd42d75
David Haralson <d.m.haralson@ieee.org>
2021-08-19 14:15:54 -0600

    Migrate Bank model functions

--- utils/dbModels.js
+++ utils/dbModels.js
@@ -1,9 +1,26 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"),
+  config = require("../config/config.json"),
+  Bank = require("../models/Bank.model");
 
 mongoose.connect(config.db.db, config.db.settings);
 
 const models = {
-
+  bank: {
+    getBalance: async function(discordId, currency) {
+      if (discordId.id) discordId = discordId.id;
+      let record = await Bank.aggregate([
+        { $match: { discordId, currency }},
+        { $group: { _id: null, balance: {$sum: "$value"}}}
+      ]).exec();
+      if (record && (record.length > 0)) return {discordId, currency, balance: record[0].balance};
+      else return {discordId, currency, balance: 0};
+    },
+    addCurrency: async function(data) {
+      if (data.discordId.id) data.discordId = data.discordId.id;
+      let record = new Bank(data);
+      return await record.save();
+    }
+  }
 };
 
 module.exports = models;
