4f3491c260ac1c48fabb0793cd3daa6350a1d13e
Gaiwecoor <gaiwecoor@gmail.com>
2021-09-01 13:33:01 -0500

    Allow u.embed() to accept a User or GuildMember as the `.author`

--- utils/utils.js
+++ utils/utils.js
@@ -49,6 +49,17 @@ const utils = {
    *   You can override the color and timestamp here as well.
    */
   embed: function(data) {
+    if (data.author instanceof Discord.GuildMember) {
+      data.author = {
+        name: data.author.displayName,
+        iconURL: data.author.user.displayAvatarURL()
+      };
+    } else if (data.author instanceof Discord.User) {
+      data.author = {
+        name: data.author.username,
+        iconURL: data.author.displayAvatarURL()
+      };
+    }
     const embed = new Discord.MessageEmbed(data);
     if (!data?.color) embed.setColor(config.color);
     if (!data?.timestamp) embed.setTimestamp();
