5a0de65045b3bece0a1da607253214d9fc7b80f0
Gaiwecoor <gaiwecoor@gmail.com>
2021-10-04 10:36:45 -0500

    `/rank view` initial commit

--- modules/rank.js
+++ modules/rank.js
@@ -1,4 +1,5 @@
 const Augur = require("augurbot"),
+  Rank = require("../utils/RankInfo"),
   config = require("../config/config.json"),
   u = require("../utils/utils");
 
@@ -13,11 +14,36 @@ const Module = new Augur.Module()
 
       if (subcommand === "view") {
         // View member rankings
-      } else if (subcommand === "leaderboard") {
+        await interaction.deferReply();
         const members = interaction.guild.members.cache;
+        const member = interaction.options.getMember("user") ?? interaction.member;
+        const leaderboard = await Module.db.user.getLeaderboard({ members, member });
+        const record = leaderboard.find(r => r.discordId == member.id);
 
+        if (record) {
+          const embed = u.embed({ author: member })
+          .setTitle("LDSG Chat Season Ranking")
+          .setURL("https://my.ldsgamers.com/leaderboard")
+          .setFooter("https://my.ldsgamers.com/leaderboard")
+          .addField("Rank", `Season: ${record.rank} / ${members.size}`)
+          .addField("Level")
+          .addField("Exp.", `Season: ${record.currentXP.toLocaleString()} XP\nLifetime: ${record.totalXP.toLocaleString()} XP`);
+
+          await interaction.editReply({ embeds: [embed] });
+        } else {
+          const snark = [
+            "don't got time for dat.",
+            "ain't interested in no XP gettin'.",
+            "don't talk to me no more, so I ignore 'em."
+          ];
+          await interaction.editReply(`**${member}** ${u.rand(snark)}\n(Try \`/rank track\` if you want to participate in chat ranks!)`);
+        }
+      } else if (subcommand === "leaderboard") {
         // View leaderboard
         await interaction.deferReply();
+
+        const members = interaction.guild.members.cache;
+
         const leaderboard = await Module.db.user.getLeaderboard({
           members,
           member: interaction.member
@@ -33,6 +59,7 @@ const Module = new Augur.Module()
 
         await interaction.editReply({ embeds: [embed] });
       } else if (subcommand === "track") {
+        // Set XP tracking
         await interaction.deferReply({ ephemeral: true });
         const track = interaction.options.getBoolean("choice");
         await Module.db.user.trackXP(interaction.user, track);
