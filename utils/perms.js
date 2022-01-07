e2705c03cd61d3991be9eaae2a94f5a7109fc933
GuyInGrey <guyingreydaboss@gmail.com>
2022-01-06 21:58:51 -0700

    Fixes

--- utils/perms.js
+++ utils/perms.js
@@ -1,8 +1,8 @@
 const sf = require("../config/snowflakes.json");
 
 const perms = {
-  isAdmin: (msg) => sf.adminId.includes(msg.author.id),
-  isOwner: (msg) => msg.author.id === sf.ownerId,
+  isAdmin: (msg) => sf.adminId.includes((msg.author ?? msg.user).id),
+  isOwner: (msg) => (msg.author ?? msg.user).id === sf.ownerId,
   isMod: function(msg) {
     const roles = msg.member?.roles.cache;
     return roles?.has(sf.roles.mod) || roles?.has(sf.roles.management);
