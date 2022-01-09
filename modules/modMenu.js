d20e0647b951ba9d6602a849965e194dd8e7fcf7
David Haralson <d.m.haralson@ieee.org>
2022-01-08 23:07:21 -0700

    Reduce repetition

--- modules/modMenu.js
+++ modules/modMenu.js
@@ -17,114 +17,82 @@ function permCheck(inter) {
   );
 }
 
-async function flagUser() {
-  // Stuff goes here
-}
-
-async function userInfo() {
-  // Stuff goes here
-}
-
-async function userAvatar() {
-  // Stuff goes here
-}
-
-async function flagMessage() {
-  // Stuff goes here
-}
-
-async function pinMessage() {
-  // Stuff goes here
-}
-
-async function fullinfo() {
-  // Stuff goes here
-}
-
-async function summary() {
-  // Stuff goes here
-}
-
-async function noteUser() {
-  // Stuff goes here
-}
-
-async function renameUser() {
-  // Stuff goes here
-}
-
-async function trustUser() {
-  // Stuff goes here
-}
-
-async function trustPlusUser() {
-  // Stuff goes here
-}
-
-async function watchUser() {
-  // Stuff goes here
-}
-
-async function warnUser() {
-  // Stuff goes here
-}
-
-async function muteUser() {
-  // Stuff goes here
-}
-
-async function timeoutUser() {
-  // Stuff goes here
-}
-
-async function kickUser() {
-  // Stuff goes here
-}
-
-async function banUser() {
-  // Stuff goes here
-}
-
-async function warnMessage() {
-  // Stuff goes here
-}
-
-async function purgeChannel() {
-  // Stuff goes here
-}
-
-async function announceMessage() {
-  // Stuff goes here
-}
-
+const processes = {
+  flagUser: async function() {
+    // Stuff goes here
+  },
+  userInfo: async function() {
+    // Stuff goes here
+  },
+  userAvatar: async function() {
+    // Stuff goes here
+  },
+  flagMessage: async function() {
+    // Stuff goes here
+  },
+  pinMessage: async function() {
+    // Stuff goes here
+  },
+  fullinfo: async function() {
+    // Stuff goes here
+  },
+  summary: async function() {
+    // Stuff goes here
+  },
+  noteUser: async function() {
+    // Stuff goes here
+  },
+  renameUser: async function() {
+    // Stuff goes here
+  },
+  trustUser: async function() {
+    // Stuff goes here
+  },
+  trustPlusUser: async function() {
+    // Stuff goes here
+  },
+  watchUser: async function() {
+    // Stuff goes here
+  },
+  warnUser: async function() {
+    // Stuff goes here
+  },
+  muteUser: async function() {
+    // Stuff goes here
+  },
+  timeoutUser: async function() {
+    // Stuff goes here
+  },
+  kickUser: async function() {
+    // Stuff goes here
+  },
+  banUser: async function() {
+    // Stuff goes here
+  },
+  warnMessage: async function() {
+    // Stuff goes here
+  },
+  purgeChannel: async function() {
+    // Stuff goes here
+  },
+  announceMessage: async function() {
+    // Stuff goes here
+  }
+};
 
 const allMenuItems = new u.Collection()
-.set('flagUser', { key: 0, data: menuOptions.flagUser, process: flagUser })
-.set('userInfo', { key: 0, data: menuOptions.userInfo, process: userInfo })
-.set('userAvatar', { key: 0, data: menuOptions.userAvatar, process: userAvatar })
-.set('flagMessage', { key: isMsg, data: menuOptions.flagMessage, process: flagMessage })
-.set('pinMessage', { key: isMsg, data: menuOptions.pinMessage, process: pinMessage })
-.set('fullinfo', { key: isMod, data: menuOptions.fullinfo, process: fullinfo })
-.set('summary', { key: isMod, data: menuOptions.summary, process: summary })
-.set('noteUser', { key: isMod, data: menuOptions.noteUser, process: noteUser })
-.set('renameUser', { key: isMod, data: menuOptions.renameUser, process: renameUser })
-.set('trustUser', { key: isMod, data: menuOptions.trustUser, process: trustUser })
-.set('trustPlusUser', { key: isMod, data: menuOptions.trustPlusUser, process: trustPlusUser })
-.set('watchUser', { key: isMod, data: menuOptions.watchUser, process: watchUser })
-.set('warnUser', { key: isMod, data: menuOptions.warnUser, process: warnUser })
-.set('muteUser', { key: isMod, data: menuOptions.muteUser, process: muteUser })
-.set('timeoutUser', { key: isMod, data: menuOptions.timeoutUser, process: timeoutUser })
-.set('kickUser', { key: isMod, data: menuOptions.kickUser, process: kickUser })
-.set('banUser', { key: isMod, data: menuOptions.banUser, process: banUser })
-.set('warnMessage', { key: isMod + isMsg, data: menuOptions.warnMessage, process: warnMessage })
-.set('purgeChannel', { key: isMod + isMsg, data: menuOptions.purgeChannel, process: purgeChannel })
-.set('announceMessage', { key: isMgr + isMsg, data: menuOptions.announceMessage, process: announceMessage });
+.set(0, ['flagUser', 'userInfo', 'userAvatar'])
+.set(isMsg, ['flagMessage', 'pinMessage'])
+.set(isMod, ['fullinfo', 'summary', 'noteUser', 'renameUser', 'trustUser', 'trustPlusUser', 'watchUser', 'warnUser',
+  'muteUser', 'timeoutUser', 'kickUser', 'banUser'])
+.set(isMod + isMsg, ['warnMessage', 'purgeChannel'])
+.set(isMgr + isMsg, ['announceMessage']);
 
 async function modMenu(inter) {
   const includeKey = permCheck(inter);
   const options = [];
-  for (const [, item] of allMenuItems) {
-    if (includeKey & item.key == item.key) options.push(item.data);
+  for (const [key, item] of allMenuItems) {
+    if (includeKey & key == key) options.push(menuOptions[item]);
   }
 
   // Sort menu items (alphabetically?)
@@ -136,7 +104,7 @@ async function modMenu(inter) {
   // Issue confirm dialog? Or should this (where applicable) be in the process function?
   // const confirm = u.confirmInteraction(inter, "Do you want to do the thing?");
   // Call function based on selection
-  // await allMenuItems.get(selection).process()
+  // await processes[selection]()
 }
 
 const Module = new Augur.Module()
