90acb8798fd27df66b60ceba5ac241fd4d8e08cf
GuyInGrey <guyingreydaboss@gmail.com>
2022-01-18 21:14:18 -0700

    Basic slash registry

---
+++ modules/vote.js
@@ -0,0 +1,10 @@
+const Augur = require("augurbot"),
+  p = require("../utils/perms"),
+  sf = require("../config/snowflakes"),
+  u = require("../utils/utils"),
+  c = require("../utils/modCommon"),
+  Discord = require("discord.js");
+
+const Module = new Augur.Module();
+
+module.exports = Module;
