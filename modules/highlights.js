880035b3401c824227ccecccda35dc849b20c9d3
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2022-02-27 16:25:33 -0500

    Bug fixes, reworked loop system

--- modules/highlights.js
+++ modules/highlights.js
@@ -2,27 +2,37 @@ const Augur = require('augurbot'),
   sf = require("../config/snowflakes.json"),
   Module = new Augur.Module();
 Module.addCommand({ name: "fetchhighlights",
-  permissions: (msg) => msg.member?.roles.cache.hasAny([sf.roles.team, sf.roles.management, sf.roles.manager]),
+  permissions: (msg) => msg.guild?.id == sf.ldsg && msg.member.roles.cache.hasAny([sf.roles.team, sf.roles.management, sf.roles.manager]),
   process: async (msg) => {
     const date = new Date();
     const after = new Date(date.getFullYear(), date.getMonth(), 1);
     const channel = msg.guild.channels.cache.get(sf.channels.highlightsubmissions); // #highlight-submissions
     const messages = [];
     let lastId;
-    for (let i = 0; i < 5; i++) {
-      setTimeout(async () => {
-        const fetched = await channel.messages.fetch({ limit: 100, before: lastId });
-        fetched.filter(a => a.createdAt >= after);
-        messages.push(...fetched.map(a => a));
-        lastId = fetched.last().id;
-        if (messages.size != 100 || messages >= 5) return;
-      }, 5000 * i);
-    }
-    let results = messages.filter(a => a.attachments.size > 0);
-    if (results) {
-      results = results.map(a => a.attachments.map(b => (`{"url": "${b.url}", "name": "${b.name}", "author": "${a.author.username}"}`)));
-      const final = Buffer.from(`[${results.join(',\n')}]`, 'utf8');
-      msg.author.send({ files: [{ attachment: final, name: `${after.toDateString()} Highlight Reel.json` }] });
+    await new Promise(res => {
+      const loop = async () => {
+        setTimeout(async () => {
+          console.log('loop started');
+          let fetched = await channel.messages.fetch({ limit: 100, before: lastId });
+          fetched = fetched.filter(a => a.createdTimestamp >= after.getTime()).map(a => a);
+          if (fetched.length != 100) console.log('less');
+          lastId = fetched[fetched.length - 1].id;
+          messages.push(...fetched);
+          if (fetched.length != 100) res();
+          else loop();
+        }, 3000);
+      };
+      loop();
+    });
+    if (messages.length > 0) {
+      let results = messages.filter(a => a.attachments.size > 0);
+      let otherLinks = messages.filter(a => a.content?.includes('https://'));
+      if (results.length > 0 || otherLinks.length > 0) {
+        results = results.map(a => a.attachments.map(b => (`{"url": "${b.url}", "name": "${b.name}", "author": "${a.author.username}"}`)));
+        otherLinks = otherLinks.map(a => `{"content": "${a.content}", "author": "${a.author.username}"}`);
+        const final = Buffer.from(`{"files": [${results.join(',\n')}],\n"urls": [\n${otherLinks.join(',\n')}]}`, 'utf8');
+        msg.author.send({ files: [{ attachment: final, name: `${after.toDateString()} Highlight Reel.json` }] });
+      } else {msg.reply("I couldn't find any new submissions!");}
     } else {msg.reply("I couldn't find any new submissions!");}
   }
 });
