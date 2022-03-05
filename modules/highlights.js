8d8f1f4f58766b44c69b8242381209df70ff4c6f
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2022-03-05 13:24:45 -0500

    Date input

--- modules/highlights.js
+++ modules/highlights.js
@@ -4,16 +4,16 @@ const Augur = require('augurbot'),
   Module = new Augur.Module();
 Module.addCommand({ name: "fetchhighlights",
   permissions: (msg) => msg.guild?.id == sf.ldsg && msg.member.roles.cache.hasAny(sf.roles.team, sf.roles.management, sf.roles.manager),
-  process: async (msg) => {
-    const date = new Date();
-    const after = new Date(date.getFullYear(), date.getMonth(), 1);
+  process: async (msg, suffix) => {
+    const after = Date.parse(suffix);
+    if (isNaN(after)) return msg.reply('Thats an invalid date.');
     const channel = msg.guild.channels.cache.get(sf.channels.highlightsubmissions); // #highlight-submissions
     const messages = [];
     let lastId;
     let fetched;
     do {
       fetched = await channel.messages.fetch({ limit: 100, before: lastId });
-      fetched = fetched.filter(a => a.createdTimestamp >= after.getTime()).map(a => a);
+      fetched = fetched.filter(a => a.createdTimestamp >= after).map(a => a);
       lastId = fetched[fetched.length - 1].id;
       messages.push(...fetched);
     } while (fetched.length == 100);
