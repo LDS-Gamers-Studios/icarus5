2c56c0403dbd462e9074dac11ada1ae353329f0a
GuyInGrey <guyingreydaboss@gmail.com>
2022-03-18 06:40:06 -0600

    Translator

---
+++ modules/sga.js
@@ -0,0 +1,67 @@
+/* eslint-disable no-multi-spaces */
+const Augur = require("augurbot"),
+  { MessageActionRow, MessageButton } = require('discord.js');
+
+const dict = {
+  "a": "ᔑ",  "b": "ʖ",  "c": "ᓵ",  "d": "↸",  "e": "ŀ",
+  "f": "⎓",  "g": "ㅓ",  "h": "〒",  "i": "╎",   "j": "፧",
+  "k": "ꖌ",  "l": "ꖎ",  "m": "ᒲ",  "n": "リ",  "o": "フ",
+  "p": "¡",  "q": "ᑑ",  "r": "።",  "s": "ነ",  "t": "ﬧ",
+  "u": "⚍",  "v": "⍊",  "w": "∴",  "x": "∕",  "y": "॥",
+  "z": "∩",   zeroWidthSpace: '​',
+};
+
+const translate = (sga) => {
+  let to = "";
+
+  let upper = false;
+
+  for (let i = 0; i < sga.length; i++) {
+    const c = sga.charAt(i);
+    if (c === dict.zeroWidthSpace) {
+      upper = true;
+      continue;
+    }
+
+    const f = Object.keys(dict).find(key => dict[key] === c);
+    to += f ? (upper ? f.toUpperCase() : f) : (upper ? c.toUpperCase() : c);
+    upper = false;
+  }
+
+  return to;
+};
+
+const handleMessage = (msg) => {
+  if (msg.author.bot || !([...msg.content].some(char => Object.values(dict).includes(char)))) {
+    return;
+  }
+
+  const row = new MessageActionRow()
+  .addComponents(
+    new MessageButton()
+      .setCustomId('sgaTranslate')
+      .setLabel('Translate')
+      .setStyle('PRIMARY'),
+  );
+
+  msg.reply({
+    components: [row]
+  });
+};
+
+const handleButton = (inter) => {
+  if (!inter.isButton() || inter.customId !== "sgaTranslate") return;
+  inter.message.fetchReference()
+    .then(msg => {
+      const translated = translate(msg.content);
+      inter.reply({ content: translated, ephemeral: true });
+    }).catch(() => {
+      inter.reply({ content: "It appears the message was deleted.", ephemeral: true });
+    });
+};
+
+const Module = new Augur.Module()
+  .addEvent("messageCreate", handleMessage)
+  .addEvent("interactionCreate", handleButton);
+
+module.exports = Module;
