dcaf8723f03593f4926b61d43b8c57846289b907
David Haralson <d.m.haralson@ieee.org>
2022-09-05 21:30:16 -0600

    Refactor `int` object names to `inter`

--- modules/games.js
+++ modules/games.js
@@ -14,14 +14,14 @@ let gameDefaults = new u.Collection();
 
 const Module = new Augur.Module();
 
-/** @param {discord.CommandInteraction} int @param {string} game*/
-function currentPlayers(int, game) {
-  const players = int.guild.members.cache.map(m => {
+/** @param {discord.CommandInteraction} inter @param {string} game*/
+function currentPlayers(inter, game) {
+  const players = inter.guild.members.cache.map(m => {
     if (m.user.bot) return null;
     const presence = m.presence?.activities?.find(a => a.type == "PLAYING" && a.name.toLowerCase().startsWith(game.toLowerCase()));
     return presence ? `• ${m}` : null;
   }).filter(p => p != null).sort((a, b) => a.localeCompare(b));
-  return u.embed().setTitle(`${int.guild.name} members currently playing ${game}`).setDescription(players.length > 0 ? players.join('\n') : `I couldn't find any members playing ${game}`);
+  return u.embed().setTitle(`${inter.guild.name} members currently playing ${game}`).setDescription(players.length > 0 ? players.join('\n') : `I couldn't find any members playing ${game}`);
 }
 
 async function updateFactionStatus() {
@@ -41,13 +41,13 @@ async function updateFactionStatus() {
   } catch (e) { u.errorHandler(e, "Elite Channel Update Error"); }
 }
 
-/** @param {discord.CommandInteraction} int */
-async function getPlaying(int) {
-  const game = int.options.getString("game") ?? gameDefaults.get(int.channel.id)?.game;
-  if (game) return int.reply({ embeds: [currentPlayers(int, game)], ephemeral: true });
+/** @param {discord.CommandInteraction} inter */
+async function getPlaying(inter) {
+  const game = inter.options.getString("game") ?? gameDefaults.get(inter.channel.id)?.game;
+  if (game) return inter.reply({ embeds: [currentPlayers(inter, game)], ephemeral: true });
   // List *all* games played
   const games = new u.Collection();
-  for (const [, member] of int.guild.members.cache) {
+  for (const [, member] of inter.guild.members.cache) {
     if (member.user.bot) continue;
     const playing = member.presence?.activities?.find(a => a.type == "PLAYING");
     if (playing && !games.has(playing.name)) games.set(playing.name, { game: playing.name, players: 0 });
@@ -60,19 +60,19 @@ async function getPlaying(int) {
   }).toJSON();
   const s = gameList.length > 0 ? 's' : '';
   const embed = u.embed().setTimestamp()
-    .setTitle(`Currently played game${s} in ${int.guild.name}`)
-    .setDescription(`The top ${Math.min(gameList.length, 25)} game${s} currently being played in ${int.guild.name}:`);
+    .setTitle(`Currently played game${s} in ${inter.guild.name}`)
+    .setDescription(`The top ${Math.min(gameList.length, 25)} game${s} currently being played in ${inter.guild.name}:`);
   if (gameList.length > 0) gameList.map((g, i) => i < 25 ? embed.addFields({ name: g.game, value: `${g.players}` }) : null);
   else embed.setDescription("Well, this is awkward ... I couldn't find any games with more than one member playing.");
-  int.reply({ embeds: [embed], ephemeral: true });
+  inter.reply({ embeds: [embed], ephemeral: true });
 }
 
-/** @param {discord.CommandInteraction} int */
-async function chess(int) {
-  const user = int.options.getMember('user');
-  let name = int.options.getString('username');
+/** @param {discord.CommandInteraction} inter */
+async function chess(inter) {
+  const user = inter.options.getMember('user');
+  let name = inter.options.getString('username');
   if (user) name = (await Module.db.ign.find(user.id, 'chess'))?.ign;
-  else if (!name) name = (await Module.db.ign.find(int.user.id, 'chess'))?.ign;
+  else if (!name) name = (await Module.db.ign.find(inter.user.id, 'chess'))?.ign;
   if (name) {
     try {
       let result = await chessAPI.getPlayerCurrentDailyChess(encodeURIComponent(name));
@@ -99,38 +99,38 @@ async function chess(int) {
             .addField("Chess Daily", (overall || latest || best) ? `${overall}${latest}${best}` : "No available stats")
             .addField("Puzzle Rush", puzzle ? `Total Attempts: ${puzzle["total_attempts"]}\nHigh Score: ${puzzle.score}` : "No available stats")
             .addField("Tactics", tactics ? `Highest Rating: ${tactics.highest.rating}\nLowest Rating: ${tactics.lowest.rating}` : "No available stats");
-          return int.reply({ embeds: [embed] });
+          return inter.reply({ embeds: [embed] });
         }
       }
       if (games.length == 0) embed.setDescription(`No active games found for ${name}`);
       if (games.length > 25) embed.setDescription(`${name}'s first 25 active games:`);
       else embed.setDescription(`${name}'s active games:`);
-      int.reply({ embeds: [embed] });
+      inter.reply({ embeds: [embed] });
     } catch (error) {
       if (error.message == "Not Found" && error.statusCode == 404) {
-        int.reply({ content: `I couldn't find a profile for \`${name}\`.`, ephemeral: true });
-      } else { u.errorHandler(error, int); }
+        inter.reply({ content: `I couldn't find a profile for \`${name}\`.`, ephemeral: true });
+      } else { u.errorHandler(error, inter); }
     }
   } else {
-    int.reply({ content: "I couldn't find a saved IGN for them.", ephemeral: true });
+    inter.reply({ content: "I couldn't find a saved IGN for them.", ephemeral: true });
   }
 }
 
-/** @param {discord.CommandInteraction} int */
-async function elite(int) {
-  const starSystem = int.options.getString('system-name') ? await eliteAPI.getSystemInfo(int.options.getString('system-name')) : null;
+/** @param {discord.CommandInteraction} inter */
+async function elite(inter) {
+  const starSystem = inter.options.getString('system-name') ? await eliteAPI.getSystemInfo(inter.options.getString('system-name')) : null;
   const embed = u.embed().setThumbnail("https://i.imgur.com/Ud8MOzY.png").setAuthor({ name: "EDSM", iconURL: "https://i.imgur.com/4NsBfKl.png" });
   const getStatus = async () => {
     const status = await eliteAPI.getEliteStatus();
-    return int.reply({ content: `The Elite: Dangerous servers are ${status.type == 'success' ? "online" : "offline"}`, ephemeral: true });
+    return inter.reply({ content: `The Elite: Dangerous servers are ${status.type == 'success' ? "online" : "offline"}`, ephemeral: true });
   };
   const getTime = async () => {
     const d = new Date();
     const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    return int.reply(`The current date/time in Elite is ${monthNames[d.getUTCMonth()]} ${d.getUTCDate()}, ${(d.getUTCFullYear() + 1286)}, ${d.getUTCHours()}:${d.getUTCMinutes()}. (UTC + 1286 years)`);
+    return inter.reply(`The current date/time in Elite is ${monthNames[d.getUTCMonth()]} ${d.getUTCDate()}, ${(d.getUTCFullYear() + 1286)}, ${d.getUTCHours()}:${d.getUTCMinutes()}. (UTC + 1286 years)`);
   };
   const getSystem = async () => {
-    if (!starSystem) return int.reply({ content: "I couldn't find a system with that name.", ephemeral: true });
+    if (!starSystem) return inter.reply({ content: "I couldn't find a system with that name.", ephemeral: true });
     embed.setTitle(starSystem.name)
       .setURL(`https://www.edsm.net/en/system/id/${starSystem.id}/name`)
       .addField('Permit Required?', starSystem.requirePermit ? "Yes" : "No", true);
@@ -142,11 +142,11 @@ async function elite(int) {
     } else {
       embed.addField("Uninhabited System", "No faction information available.", true);
     }
-    return int.reply({ embeds: [embed] });
+    return inter.reply({ embeds: [embed] });
   };
   const getStations = async () => {
-    if (!starSystem) return int.reply({ content: "I couldn't find a system with that name.", ephemeral: true });
-    if (starSystem.stations.length <= 0) return int.reply({ content: "I couldn't find any stations in that system.", ephemeral: true });
+    if (!starSystem) return inter.reply({ content: "I couldn't find a system with that name.", ephemeral: true });
+    if (starSystem.stations.length <= 0) return inter.reply({ content: "I couldn't find any stations in that system.", ephemeral: true });
     embed.setTitle(starSystem.name).setURL(starSystem.stationsURL);
 
     const stationList = new Map();
@@ -174,11 +174,11 @@ async function elite(int) {
 
     // Letting the user know there were more than 25
     if (stationList.size > 25) embed.setFooter({ text: "Some stations were filtered out because the limit was exceeded.", iconURL: "https://i.imgur.com/vYPj8iX.png" });
-    int.reply({ embeds: [embed] });
+    inter.reply({ embeds: [embed] });
   };
   const getFactions = () => {
-    if (!starSystem) return int.reply({ content: "I couldn't find a system with that name.", ephemeral: true });
-    if (starSystem.factions.length < 1) return int.reply({ content: "I couldn't find any factions in that system.", ephemeral: true });
+    if (!starSystem) return inter.reply({ content: "I couldn't find a system with that name.", ephemeral: true });
+    if (starSystem.factions.length < 1) return inter.reply({ content: "I couldn't find any factions in that system.", ephemeral: true });
     embed.setTitle(starSystem.name).setURL(starSystem.factionsURL);
 
     for (const faction of starSystem.factions) {
@@ -187,11 +187,11 @@ async function elite(int) {
       embed.addField(faction.name + (starSystem.information && (faction.name === starSystem.information.faction) ? " (Controlling)" : "") + " " + influence + "%",
         "State: " + faction.state + "\nGovernment: " + faction.allegiance + " - " + faction.government + "\n[Link](" + url + ")", true);
     }
-    return int.reply({ embeds: [embed] });
+    return inter.reply({ embeds: [embed] });
   };
   const getBodies = () => {
-    if (!starSystem) return int.reply({ content: "I couldn't find a system with that name.", ephemeral: true });
-    if (starSystem.bodies.length < 1) return int.reply({ content: "I couldn't find any bodies in that system.", ephemeral: true });
+    if (!starSystem) return inter.reply({ content: "I couldn't find a system with that name.", ephemeral: true });
+    if (starSystem.bodies.length < 1) return inter.reply({ content: "I couldn't find any bodies in that system.", ephemeral: true });
     embed.setTitle(starSystem.name).setURL(starSystem.bodiesURL);
 
     for (const body of starSystem.bodies) {
@@ -199,9 +199,9 @@ async function elite(int) {
       const distance = Math.round(body.distanceToArrival * 10) / 10;
       embed.addField(body.name, body.type + scoopable + "\n" + distance + " ls", true);
     }
-    return int.reply({ embeds: [embed] });
+    return inter.reply({ embeds: [embed] });
   };
-  const info = int.options.getString('info');
+  const info = inter.options.getString('info');
   switch (info) {
   case "status": return getStatus();
   case "time": return getTime();
@@ -212,75 +212,75 @@ async function elite(int) {
   }
 }
 
-/** @param {discord.CommandInteraction} int */
-async function minecraftSkin(int) {
-  const user = int.options.getMember('user');
-  let name = int.options.getString('username');
-  if (user) name ??= await Module.db.ign.find(user.id ?? int.user.id, 'minecraft')?.ign;
-  if (user && !name) return int.reply({ content: `${user} has not set a Minecraft name in their IGN`, ephemeral: true });
-  if (!name) return int.reply({ content: "I need a Discord user or Minecraft username to look up.", ephemeral: true });
+/** @param {discord.CommandInteraction} inter */
+async function minecraftSkin(inter) {
+  const user = inter.options.getMember('user');
+  let name = inter.options.getString('username');
+  if (user) name ??= await Module.db.ign.find(user.id ?? inter.user.id, 'minecraft')?.ign;
+  if (user && !name) return inter.reply({ content: `${user} has not set a Minecraft name in their IGN`, ephemeral: true });
+  if (!name) return inter.reply({ content: "I need a Discord user or Minecraft username to look up.", ephemeral: true });
   const uuid = await eliteAPI.axiosRequest({ hostname: `https://api.mojang.com/users/profiles/minecraft/${name}` });
-  if (!uuid?.id) return int.reply({ content: "I couldn't find that player", ephemeral: true });
+  if (!uuid?.id) return inter.reply({ content: "I couldn't find that player", ephemeral: true });
   const skinUrl = `https://visage.surgeplay.com/full/512/${uuid.id}`;
-  int.reply({ files: [{ attachment: skinUrl, name: `${name}.png` }] });
+  inter.reply({ files: [{ attachment: skinUrl, name: `${name}.png` }] });
 }
 
-/** @param {discord.CommandInteraction} int */
-async function destiny(int) {
+/** @param {discord.CommandInteraction} inter */
+async function destiny(inter) {
   const setClan = async () => {
-    if (!perms.isAdmin(int) && !int.member.roles.cache.hasAny([sf.roles.destinyclansadmin, sf.roles.destinyclansmanager])) return int.reply({ content: `Only <@&${sf.roles.destinyclansadmin}> and above can use this command!`, ephemeral: true });
-    const user = int.options.getMember('user');
-    const clan = int.options.getString('clan');
-    const remove = int.options.getBoolean('remove') ?? false;
-    if (!clan && !remove) return int.reply({ content: `I need a clan to ${remove ? "remove them from" : "add them to!"}`, ephemeral: true });
+    if (!perms.isAdmin(inter) && !inter.member.roles.cache.hasAny([sf.roles.destinyclansadmin, sf.roles.destinyclansmanager])) return inter.reply({ content: `Only <@&${sf.roles.destinyclansadmin}> and above can use this command!`, ephemeral: true });
+    const user = inter.options.getMember('user');
+    const clan = inter.options.getString('clan');
+    const remove = inter.options.getBoolean('remove') ?? false;
+    if (!clan && !remove) return inter.reply({ content: `I need a clan to ${remove ? "remove them from" : "add them to!"}`, ephemeral: true });
     try {
       if (!remove) {
-        const channel = int.guild.channels.cache.get(sf.channels.destiny[clan]);
+        const channel = inter.guild.channels.cache.get(sf.channels.destiny[clan]);
         const has = channel.permissionOverwrites.cache.has(user.id);
-        if (!channel) return int.reply({ content: "sorry, I couldn't fetch the right channel. Try again in a bit?", ephemeral: true });
-        if (has) return int.reply({ content: `${user} is already in that clan!`, ephemeral: true });
+        if (!channel) return inter.reply({ content: "sorry, I couldn't fetch the right channel. Try again in a bit?", ephemeral: true });
+        if (has) return inter.reply({ content: `${user} is already in that clan!`, ephemeral: true });
         await channel.permissionOverwrites.create(user, { "VIEW_CHANNEL": true });
         await channel.send(`Welcome to the clan, ${user}!`);
-        return int.reply({ content: `Added ${user} to the ${clan} clan!`, ephemeral: true });
+        return inter.reply({ content: `Added ${user} to the ${clan} clan!`, ephemeral: true });
       } else {
         const removed = [];
         if (clan) {
-          const channel = int.guild.channels.cache.get(sf.channels.destiny[clan]);
+          const channel = inter.guild.channels.cache.get(sf.channels.destiny[clan]);
           const has = channel.permissionOverwrites.cache.has(user.id);
-          if (!channel) return int.reply({ content: "sorry, I couldn't fetch the right channel. Try again in a bit?", ephemeral: true });
-          if (!has) return int.reply({ content: `${user} isn't in that clan!`, ephemeral: true });
+          if (!channel) return inter.reply({ content: "sorry, I couldn't fetch the right channel. Try again in a bit?", ephemeral: true });
+          if (!has) return inter.reply({ content: `${user} isn't in that clan!`, ephemeral: true });
           await channel.permissionOverwrites.delete(user);
           removed.push(channel.toString());
         } else {
           for (const id in sf.channels.destiny) {
-            const channel = int.guild.channels.cache.get(sf.channels.destiny[id]);
+            const channel = inter.guild.channels.cache.get(sf.channels.destiny[id]);
             if (channel?.permissionOverwrites.cache.has(user.id)) {
               await channel.permissionOverwrites.delete(user);
               removed.push(channel.toString());
             }
           }
         }
-        if (removed.length == 0) return int.reply({ content: `${user} isn't in a clan!`, ephemeral: true });
-        return int.reply({ content: `Removed ${user} from the ${removed.join(', ')} clan(s).`, ephemeral: true });
+        if (removed.length == 0) return inter.reply({ content: `${user} isn't in a clan!`, ephemeral: true });
+        return inter.reply({ content: `Removed ${user} from the ${removed.join(', ')} clan(s).`, ephemeral: true });
       }
     } catch (error) {
-      u.errorHandler(error, int);
+      u.errorHandler(error, inter);
     }
   };
   const setValiant = async () => {
-    if (!perms.isAdmin(int) && !int.member.roles.cache.hasAny([sf.roles.destinyclansmanager, sf.roles.destinyvaliantadmin])) return int.reply({ content: `Only <@&${sf.roles.destinyvaliantadmin}> and above can use this command.`, ephemeral: true });
-    const user = int.options.getMember('user');
-    const remove = int.options.getBoolean('remove') ?? false;
+    if (!perms.isAdmin(inter) && !inter.member.roles.cache.hasAny([sf.roles.destinyclansmanager, sf.roles.destinyvaliantadmin])) return inter.reply({ content: `Only <@&${sf.roles.destinyvaliantadmin}> and above can use this command.`, ephemeral: true });
+    const user = inter.options.getMember('user');
+    const remove = inter.options.getBoolean('remove') ?? false;
     try {
       const has = user.roles.cache.has(sf.roles.destinyvaliant);
-      if ((has && !remove) || (!has && remove)) return int.reply({ content: `${user} ${remove ? "doesn't have the role yet." : "already has the role."}`, ephemeral: true });
+      if ((has && !remove) || (!has && remove)) return inter.reply({ content: `${user} ${remove ? "doesn't have the role yet." : "already has the role."}`, ephemeral: true });
       await user.roles[remove ? "remove" : "add"](sf.roles.destinyvaliant);
-      return int.reply({ content: `${user} was ${remove ? "removed from" : "added to"} the <@${sf.roles.destinyvaliant}> role`, ephemeral: true });
+      return inter.reply({ content: `${user} was ${remove ? "removed from" : "added to"} the <@${sf.roles.destinyvaliant}> role`, ephemeral: true });
     } catch (error) {
-      u.errorHandler(error, int);
+      u.errorHandler(error, inter);
     }
   };
-  switch (int.options.getString('action')) {
+  switch (inter.options.getString('action')) {
   case "clan": return setClan();
   case "valiant": return setValiant();
   }
@@ -288,13 +288,13 @@ async function destiny(int) {
 
 Module.addInteractionCommand({ name: "game",
   commandId: sf.commands.slashGames,
-  process: async (int) => {
-    switch (int.options.getSubcommand()) {
-    case "chess": return chess(int);
-    case "destiny": return destiny(int);
-    case "elite": return elite(int);
-    case "minecraft-skin": return minecraftSkin(int);
-    case "playing": return getPlaying(int);
+  process: async (inter) => {
+    switch (inter.options.getSubcommand()) {
+    case "chess": return chess(inter);
+    case "destiny": return destiny(inter);
+    case "elite": return elite(inter);
+    case "minecraft-skin": return minecraftSkin(inter);
+    case "playing": return getPlaying(inter);
     }
   }
 })
